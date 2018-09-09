894314fac0df3fa68cae4b6f75d595099e9840ba
Ahmed Madi <eng.ahmed-madi@hotmail.com>
2018-09-09 09:15:11 +0000

    project transfer edits 9/9

--- erpnext/projects/doctype/project0/test_project.js
+++ erpnext/projects/doctype/project0/test_project.js
@@ -2,15 +2,15 @@
 // rename this file from _test_[name] to test_[name] to activate
 // and remove above this line
 
-QUnit.test("test: Budget", function (assert) {
+QUnit.test("test: Project", function (assert) {
 	let done = assert.async();
 
 	// number of asserts
 	assert.expect(1);
 
 	frappe.run_serially([
-		// insert a new Budget
-		() => frappe.tests.make('Budget', [
+		// insert a new Project
+		() => frappe.tests.make('Project', [
 			// values to be set
 			{key: 'value'}
 		]),
