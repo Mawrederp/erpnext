f9b8285dfe1a73243743f7ffd06e9c2f12f4d92d
Ubuntu <ubuntu@ip-172-31-32-104.us-east-2.compute.internal>
2019-03-04 07:58:12 +0000

    add list preview

--- erpnext/hr/doctype/leave_application/leave_application_list.js
+++ erpnext/hr/doctype/leave_application/leave_application_list.js
@@ -13,5 +13,33 @@ frappe.listview_settings['Leave Application'] = {
 	get_indicator: function(doc) {
 		return [__(doc.status),frappe.utils.guess_colour(doc.status),
 			"status,=," + doc.status];
+	},
+	onload: function (listview) {
+		var arr=[]
+		frappe.route_options = {
+			
+		};
+
+		frappe.call({
+			method:"frappe.client.get_list",
+			args:{
+				doctype:"User Notification",		
+				filters: {
+					"target_doctype": 'Leave Application',
+					"status": 'Active',
+					"user": frappe.user.name
+				},		
+				fields: "target_docname",
+			},
+			callback: function(r) {
+				for(var i=0;i<r.message.length;i++){
+					arr.push(r.message[i].target_docname)
+				}
+				frappe.route_options = {
+					"name": ["in", arr]
+				};				
+			}
+		});
+
 	}
-	}
+}
--- erpnext/stock/doctype/material_request/material_request_list.js
+++ erpnext/stock/doctype/material_request/material_request_list.js
@@ -16,5 +16,33 @@ frappe.listview_settings['Material Request'] = {
 				return [__("Issued"), "green", "per_ordered,=,100"];
 			}
 		}
+	},
+	onload: function (listview) {
+		var arr=[]
+		frappe.route_options = {
+			
+		};
+
+		frappe.call({
+			method:"frappe.client.get_list",
+			args:{
+				doctype:"User Notification",		
+				filters: {
+					"target_doctype": 'Material Request',
+					"status": 'Active',
+					"user": frappe.user.name
+				},		
+				fields: "target_docname",
+			},
+			callback: function(r) {
+				for(var i=0;i<r.message.length;i++){
+					arr.push(r.message[i].target_docname)
+				}
+				frappe.route_options = {
+					"name": ["in", arr]
+				};				
+			}
+		});
+
 	}
 };
